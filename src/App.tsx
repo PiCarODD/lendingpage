49a207e11892e6253edb5b1179559b88216ca9b6
PiCarODD <heinjame@gmail.com>
2025-08-29 02:51:28 +0700

    added speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NextecAsiaLanding from './components/NextecAsiaLanding'
 import './i18n' // Import i18n configuration
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <div className="App">
       <NextecAsiaLanding />
+      {/* Vercel Speed Insights - Automatically tracks Core Web Vitals and performance metrics */}
+      <SpeedInsights />
     </div>
   )
 }
